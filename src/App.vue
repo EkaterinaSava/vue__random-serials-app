354cc907493b5e5704ac22ad53396ca7e301f2c5
EkaterinaSava <ek@weare.lt>
2018-01-26 17:47:00 +0300

    ADD HelloPage component and include this component in pages

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,23 @@
 <template>
   <div id="app">
-    <!-- -->
     <nav class="navbar is-light">
       <div class="navbar-menu is-active">
         <div class="navbar-end" v-if="!signComplete">
-          <router-link to="/sign-in">
-            <button type="button" class="button" @click="switchSign('sign-in')">Войти</button>
+          <router-link to="/sign-in" class="navbar-btn">
+            <button type="button" class="button is-primary" @click="switchSign('sign-in')">Войти</button>
           </router-link>
-          <router-link to="/sign-up">
-            <button type="button" class="button" @click="switchSign('sign-up')">Зарегистрироваться</button>
+          <router-link to="/sign-up" class="navbar-btn">
+            <button type="button" class="button is-primary is-outlined" @click="switchSign('sign-up')">Зарегистрироваться</button>
           </router-link>
         </div>
         <div class="navbar-end" v-else>
-          <router-link to="/serials">
-            <button type="button" class="button">Список сериалов</button>
+          <router-link to="/serials" class="navbar-btn">
+            <button type="button" class="button is-info">Список сериалов</button>
           </router-link>
           <span class="navbar-email">{{ email }}</span>
         </div>
       </div>
     </nav>
-
-    <!-- <section class="section" v-if="!isMainPage">
-      <div class="container">
-        <sign-in v-if="sign === 'sign-in'" @addUser="isMainPage = $event.mainPage, signComplete = $event.complete, email = $event.email, uid = $event.uid"></sign-in>
-        <sign-up v-else @regSuccess="sign = $event"></sign-up>
-      </div>
-    </section>
-    <section class="section" v-else>
-      <main-page :uid="uid"></main-page>
-    </section> -->
 
     <section class="section">
       <router-view @addUser="email = $event.email, signComplete = $event.signComplete, userUid = $event.uid" :uid="userUid"></router-view>
@@ -38,17 +27,16 @@
 </template>
 
 <script>
-  import SignIn   from './components/SignIn.vue'
-  import SignUp   from './components/SignUp.vue'
-  import MainPage from './components/MainPage.vue'
+  import HelloPage from './components/HelloPage.vue'
+  import SignIn    from './components/SignIn.vue'
+  import SignUp    from './components/SignUp.vue'
+  import MainPage  from './components/MainPage.vue'
 
   export default {
     name: 'app',
 
     data () {
       return {
-        // sign: 'sign-in',
-        // isMainPage: false,
         signComplete: false,
         email: '',
         uid: '',
@@ -57,6 +45,7 @@
     },
 
     components: {
+      HelloPage,
       SignIn,
       SignUp,
       MainPage
@@ -71,8 +60,20 @@
 </script>
 
 <style lang="scss" scoped>
-  .navbar-email {
-    align-self: center;
-    padding: 0 20px;
+  .button {
+    transition: all .25s ease-out;
+  }
+  .navbar {
+    &-btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 5px;
+    }
+
+    &-email {
+      align-self: center;
+      padding: 0 20px;
+    }
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue       from 'vue'
-import VueRouter from 'vue-router'
-import App       from './App.vue'
-import MainPage  from './components/MainPage.vue'
-import SignIn    from './components/SignIn.vue'
-import SignUp    from './components/SignUp.vue'
+import Vue        from 'vue'
+import VueRouter  from 'vue-router'
+import App        from './App.vue'
+import HelloPage  from './components/HelloPage.vue'
+import MainPage   from './components/MainPage.vue'
+import SignIn     from './components/SignIn.vue'
+import SignUp     from './components/SignUp.vue'
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: [
+    { path: '/',        component: HelloPage },
     { path: '/serials', component: MainPage },
     { path: '/sign-up', component: SignUp },
     { path: '/sign-in', component: SignIn }
